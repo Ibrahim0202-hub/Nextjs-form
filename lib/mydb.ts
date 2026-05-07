4444ad77309dfc1f07b1c30b8bb3979f862d9011
Ibrahim0202-hub <ibrahimkhaleel0202@gmail.com>
2026-05-07 16:44:23 +0530

    Update mydb configuration

--- lib/mydb.ts
+++ lib/mydb.ts
@@ -1,9 +1,6 @@
 import { Pool } from "pg";
 
 export const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "mydb",
-  password: "1234",
-  port: 5432,
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
 });
